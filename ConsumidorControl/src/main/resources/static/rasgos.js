478040ea7828e7eea0a551dd673e3415c81fd652
Fabricio <fabriciocubilla24@gmail.com>
2024-09-01 02:13:58 -0400

    las cosas nuevas que agregue

---
+++ ConsumidorControl/src/main/resources/static/rasgos.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    document.getElementById("dropdown-content").classList.toggle("show");
+}
+
+// Cerrar el dropdown si se hace clic fuera de él
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
